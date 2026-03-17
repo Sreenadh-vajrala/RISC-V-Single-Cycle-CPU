9cdd3f210849150ae7df21559056a7e85d4b53d4
Sreenadh-vajrala <sreenadh.vajrala@gmail.com>
2026-03-18 00:09:39 +0530

    Delete mux2.v

--- mux2.v
+++
@@ -1,8 +0,0 @@
-// 2-to-1 Multiplexer Module
-module mux2 #(parameter WIDTH = 8) (
-    input       [WIDTH-1:0] d0, d1,
-    input       sel,
-    output      [WIDTH-1:0] y
-);
-assign y = sel ? d1 : d0;
-endmodule
